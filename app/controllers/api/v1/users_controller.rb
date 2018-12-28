0f08cade18a055dbadf90e10e3b5e98f3dbc6fd6
richardmaccaw <ricci6@gmail.com>
2018-12-28 12:26:39 +0000

    removed show for users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,15 +6,6 @@ class Api::V1::UsersController < ApplicationController
     render json: @users
     end
 
-    def show
-        @user = User.find_by(id: params[:id])
-        if @user
-            render json: @user
-        else 
-            render json: {error: 'User not found.'}, status: 404
-        end
-    end
-
     def create
         @user = User.new(user_params)
         if @user.save
